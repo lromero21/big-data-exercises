4e2ebab1541ee415a5c82003281ddc58f7993ec8
lromero <lromero@nearsoft.com>
2019-10-07 23:58:54 -0700

    Movies recommender working

--- src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommenderTest.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommenderTest.java
@@ -15,16 +15,29 @@ public class MovieRecommenderTest {
     public void testDataInfo() throws IOException, TasteException {
         //download movies.txt.gz from 
         //    http://snap.stanford.edu/data/web-Movies.html
-        MovieRecommender recommender = new MovieRecommender("/path/to/movies.txt.gz");
-        assertEquals(7911684, recommender.getTotalReviews());
-        assertEquals(253059, recommender.getTotalProducts());
+        MovieRecommender recommender = new MovieRecommender("/Desktop/LuisMario/AmazonProblem/movies.txt.gz");
+        assertEquals(7911684, recommender.getTotalReviews()); //
+        assertEquals(253059, recommender.getTotalProducts()); // product/productId:
         assertEquals(889176, recommender.getTotalUsers());
+
+        // get a token with a review and saved it if it's not already defined, but if it's already defined plus one
+        //
+
+
+        // total reviews : 7911684
+        // total products : 253059
+        // total users : 889176
 
         List<String> recommendations = recommender.getRecommendationsForUser("A141HP4LYPWMSR");
         assertThat(recommendations, hasItem("B0002O7Y8U"));
         assertThat(recommendations, hasItem("B00004CQTF"));
         assertThat(recommendations, hasItem("B000063W82"));
 
+    }
+
+    public static void main(String[] args) throws IOException, TasteException {
+        MovieRecommenderTest call = new MovieRecommenderTest();
+        call.testDataInfo();
     }
 
 }
