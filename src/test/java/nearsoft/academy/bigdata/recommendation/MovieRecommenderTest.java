3746dbb5a203ce955a3ba43fb4d182e9645959fc
lromero <lromero@nearsoft.com>
2019-10-09 13:26:08 -0700

    Changed unnecessary lines

--- src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommenderTest.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommenderTest.java
@@ -15,29 +15,14 @@ public class MovieRecommenderTest {
     public void testDataInfo() throws IOException, TasteException {
         //download movies.txt.gz from 
         //    http://snap.stanford.edu/data/web-Movies.html
-        MovieRecommender recommender = new MovieRecommender();
-        assertEquals(7911684, recommender.getTotalReviews("Users/Desktop/LuisMario/AmazonProblem/movies.txt.gz")); //
-        assertEquals(253059, recommender.getTotalProducts("Users/Desktop/LuisMario/AmazonProblem/movies.txt.gz")); // product/productId:
-        assertEquals(889176, recommender.getTotalUsers("Users/Desktop/LuisMario/AmazonProblem/movies.txt.gz"));
-
-        // get a token with a review and saved it if it's not already defined, but if it's already defined plus one
-        //
-
-
-        // total reviews : 7911684
-        // total products : 253059
-        // total users : 889176
+        MovieRecommender recommender = new MovieRecommender("/Users/lromero/Desktop/LuisMario/AmazonProblem/big-data-exercises/src/test/java/nearsoft/academy/bigdata/recommendation/movies.txt.gz");
+        assertEquals(7911684, recommender.getTotalReviews());
+        assertEquals(253059, recommender.getTotalProducts());
+        assertEquals(889176, recommender.getTotalUsers());
 
         List<String> recommendations = recommender.getRecommendationsForUser("A141HP4LYPWMSR");
         assertThat(recommendations, hasItem("B0002O7Y8U"));
         assertThat(recommendations, hasItem("B00004CQTF"));
         assertThat(recommendations, hasItem("B000063W82"));
-
     }
-
-    public static void main(String[] args) throws IOException, TasteException {
-        MovieRecommenderTest call = new MovieRecommenderTest();
-        call.testDataInfo();
-    }
-
 }
